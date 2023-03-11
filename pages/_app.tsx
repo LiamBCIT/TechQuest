4395837b7919c3a2a2003748ad37ceb451d13b36
Brayden <dudgjs1318@gmail.com>
2023-03-11 15:30:56 -0800

    replace bootstrap to tailiwnd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import React from "react";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
-import { Form, Button, Spinner } from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
 import { FormEvent, useState } from "react";
 
 export default function Home() {
@@ -46,26 +46,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>TechQuest</h1>
-        <h2>powered by GPT-3</h2>
-        <div>
+        <h1 className="text-4xl font-bold mb-4">TechQuest</h1>
+        <h2 className="text-lg font-medium mb-4">powered by GPT-3</h2>
+        <div className="mb-4">
           Enter a topic and the AI will generate a technical interview questions
         </div>
         {/* image can go here */}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Create a interview questions...</Form.Label>
-            <Form.Control
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block font-medium mb-2" htmlFor="prompt">
+              Create a interview questions...
+            </label>
+            <input
+              className="px-3 py-2 w-full border rounded-lg"
+              id="prompt"
               name="prompt"
               type="text"
               placeholder="e.g. full-stack, front-end, backend..."
               maxLength={100}
             />
-          </Form.Group>
-          <Button type="submit" className="mb-3" disabled={quoteLoading}>
-            Make me a question
-          </Button>
-        </Form>
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-indigo-500 text-white rounded-lg disabled:bg-gray-400 disabled:cursor-not-allowed"
+            disabled={quoteLoading}
+          >
+            {quoteLoading ? "Loading..." : "Make me a question"}
+          </button>
+        </form>
         {quoteLoading && <Spinner animation="border" />}
         {quoteLoadingError && (
           <div>There was an error loading the interview questions</div>
