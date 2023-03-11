de5607bf039f6dba7b6e92b1988afbcf7bd2f248
Brayden <dudgjs1318@gmail.com>
2023-03-11 13:19:48 -0800

    implement chatgpt api for prompt technical interview questions

---
+++ pages/api/quest.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prompt = req.query.prompt as string;
+
+  if (!prompt) {
+    return res.status(400).json({ error: "Missing prompt" });
+  }
+
+  if (prompt.length > 100) {
+    return res.status(400).json({ error: "Prompt too long" });
+  }
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Create a technical interview question based on the following topic. \n
+    Topic: ${prompt}\n
+    Technical Interview Question:`,
+    max_tokens: 500,
+    temperature: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+  });
+
+  const quote = completion.data.choices[0].text;
+
+  res.status(200).json({ quote });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,79 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import { Form } from "react-bootstrap";
+import { FormEvent, useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 export default function Home() {
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formaData = new FormData(e.target as HTMLFormElement);
+    const prompt = formaData.get("prompt")?.toString().trim();
+
+    if (prompt) {
+      try {
+        setQuote("");
+        setQuoteLoading(true);
+        setQuoteLoadingError(false);
+        const response = await fetch(
+          "/api/quest?prompt=" + encodeURIComponent(prompt)
+        );
+        const data = await response.json();
+        console.log("data", data);
+        setQuote(data.quote);
+      } catch (error) {
+        console.error(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
+  }
+
   return (
     <>
       <Head>
         <title>TechQuest</title>
-        <meta name="description" content="A website using AI Prompts to help those in the tech industry prepare for interviews" />
+        <meta
+          name="description"
+          content="A website using AI Prompts to help those in the tech industry prepare for interviews"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <h1>TechQuest</h1>
+        <h2>powered by GPT-3</h2>
+        <div>
+          Enter a topic and the AI will generate a technical interview questions
+        </div>
+        {/* image can go here */}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Create a interview questions...</Form.Label>
+            <Form.Control
+              name="prompt"
+              type="text"
+              placeholder="e.g. full-stack, front-end, backend..."
+              maxLength={100}
+            />
+          </Form.Group>
+          <Button type="submit" className="mb-3" disabled={quoteLoading}>
+            Make me a question
+          </Button>
+        </Form>
+        {quoteLoading && <Spinner animation="border" />}
+        {quoteLoadingError && (
+          <div>There was an error loading the interview questions</div>
+        )}
+        {quote && <h5>{quote}</h5>}
       </main>
     </>
-  )
+  );
 }
